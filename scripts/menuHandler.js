84b5b2f1c4a7baedc6576dc95f8a9fd3d1f437b4
Eden <edenb.dev@gmail.com>
2024-02-14 10:23:43 +0200

    Resolve check mark styling issue in menu
    Fixes the visual bug where the check mark on the selected item in the menu wasn't receiving the correct CSS styling. The issue was caused by a recent change in the class name implemented by Spotify. This fix ensures the correct styling is applied by directly adding the necessary CSS styles, eliminating reliance on Spotify's class names.
    
    Closes #1

--- scripts/menuHandler.js
+++ scripts/menuHandler.js
@@ -119,7 +119,7 @@ class MenuHandler {
 	        li.appendChild(btn);
 	        btn.appendChild(span);
 	        if (formatedSpeed == selectedSpeed)
-	            btn.innerHTML += '<svg role="img" aria-hidden="true" viewBox="0 0 16 16" class="kPpCsU"><path d="M15.53 2.47a.75.75 0 0 1 0 1.06L4.907 14.153.47 9.716a.75.75 0 0 1 1.06-1.06l3.377 3.376L14.47 2.47a.75.75 0 0 1 1.06 0z"></path></svg>';
+	            btn.innerHTML += '<svg role="img" aria-hidden="true" viewBox="0 0 16 16"><path d="M15.53 2.47a.75.75 0 0 1 0 1.06L4.907 14.153.47 9.716a.75.75 0 0 1 1.06-1.06l3.377 3.376L14.47 2.47a.75.75 0 0 1 1.06 0z"></path></svg>';
 	        
 	        
 	        // Add item to container.
